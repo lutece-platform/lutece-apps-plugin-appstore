8f8762eddeba16d414514f636e4e5c137c478143
pierrelevy <pierrelevy@users.noreply.github.com>
2013-06-14 22:04:04 +0000

    appstore work in progress
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/applications/plugin-appstore@48839 bab10101-e421-0410-a517-8ce0973de3ef

--- src/sql/plugins/appstore/core/init_core_appstore.sql
+++ src/sql/plugins/appstore/core/init_core_appstore.sql
@@ -1,1 +1,15 @@
+--
+-- Dumping data for table core_admin_right
+--
+DELETE FROM core_admin_right WHERE id_right = 'APPSTORE_MANAGEMENT';
+INSERT INTO core_admin_right (id_right,name,level_right,admin_url,description,is_updatable,plugin_name,id_feature_group,icon_url,documentation_url, id_order ) VALUES 
+('APPSTORE_MANAGEMENT','appstore.adminFeature.manageappstore.name',1,'jsp/admin/plugins/appstore/ManageApplications.jsp','appstore.adminFeature.manageappstore.description',0,'appstore',NULL,NULL,NULL,4);
+
+
+--
+-- Dumping data for table core_user_right
+--
+DELETE FROM core_user_right WHERE id_right = 'APPSTORE_MANAGEMENT';
+INSERT INTO core_user_right (id_right,id_user) VALUES ('APPSTORE_MANAGEMENT',1);
+
 
