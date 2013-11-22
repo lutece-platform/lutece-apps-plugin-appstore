f229aece54f7bd7889294accf4b57f649f562525
pierrelevy <pierrelevy@users.noreply.github.com>
2013-11-22 16:28:07 +0000

    APPSTORE-2 : Use HttpAccess library in order to use the plugin through a proxy
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/applications/plugin-appstore@54293 bab10101-e421-0410-a517-8ce0973de3ef

--- src/java/fr/paris/lutece/plugins/appstore/service/ComponentInfoService.java
+++ src/java/fr/paris/lutece/plugins/appstore/service/ComponentInfoService.java
@@ -1,12 +1,43 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (c) 2002-2013, Mairie de Paris
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice
+ *     and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice
+ *     and the following disclaimer in the documentation and/or other materials
+ *     provided with the distribution.
+ *
+ *  3. Neither the name of 'Mairie de Paris' nor 'Lutece' nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * License 1.0
  */
 package fr.paris.lutece.plugins.appstore.service;
 
 import fr.paris.lutece.plugins.appstore.business.Component;
 import fr.paris.lutece.portal.service.util.AppLogService;
 import fr.paris.lutece.portal.service.util.AppPropertiesService;
+import fr.paris.lutece.util.httpaccess.HttpAccess;
+import fr.paris.lutece.util.httpaccess.HttpAccessException;
 
 import org.jsoup.Jsoup;
 
@@ -28,7 +59,9 @@ public class ComponentInfoService
     {
         try
         {
-            Document jarList = Jsoup.connect( NEXUS_URL + component.getArtifactId(  ) ).get(  );
+            HttpAccess httpAccess = new HttpAccess();
+            String strHtml = httpAccess.doGet( NEXUS_URL + component.getArtifactId(  ));
+            Document jarList =  Jsoup.parse( strHtml );
             Elements trList = jarList.select( "td a" );
 
             StringBuilder strJson = new StringBuilder(  );
@@ -41,7 +74,7 @@ public class ComponentInfoService
                 component.setVersion( version );
             }
         }
-        catch ( Exception e )
+        catch ( HttpAccessException e )
         {
             AppLogService.error( "AppStore - ComponentInfoService : Error retrieving release version : " + e.getMessage() , e );
             component.setVersion( "Release not found" );
