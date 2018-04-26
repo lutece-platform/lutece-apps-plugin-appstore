2bf0520c84b450ab94cfbbced3bb86da5f447a03
pierrelevy <pierrelevy@users.noreply.github.com>
2018-04-26 10:57:51 +0200

    APPSTORE-10 : Extends dependencies were missing

--- src/sql/plugins/extend/modules/comment/plugin/init_db_extend_comment_appstore.sql
+++ src/sql/plugins/extend/modules/comment/plugin/init_db_extend_comment_appstore.sql
@@ -1,2 +1,3 @@
 
-INSERT INTO extend_comment_config (id_extender, is_moderated, nb_comments, id_mailing_list, authorize_sub_comments, use_bbcode, admin_badge, message_comment_created) VALUES (3,0,10,-1,0,1,'<p><span class=\"badge badge-warning\">Admin</span></p>','<p>Votre commentaire a bien &eacute;t&eacute; cr&eacute;&eacute;.<br />Il sera disponible une fois qu\'il aura &eacute;t&eacute; valid&eacute; pas un administrateur.</p>');
+INSERT INTO extend_comment_config (id_extender, is_moderated, nb_comments, id_mailing_list, authorize_sub_comments, use_bbcode, admin_badge, message_comment_created) 
+VALUES (3,0,10,-1,0,1,'<p><span class=\"badge badge-warning\">Admin</span></p>','<p>Votre commentaire a bien &eacute;t&eacute; cr&eacute;&eacute;.<br />Il sera disponible une fois qu\'il aura &eacute;t&eacute; valid&eacute; pas un administrateur.</p>');
--- src/sql/plugins/extend/modules/rating/plugin/init_db_extend_rating_appstore.sql
+++ src/sql/plugins/extend/modules/rating/plugin/init_db_extend_rating_appstore.sql
@@ -1,2 +1,3 @@
-INSERT INTO extend_rating_config (id_extender, id_mailing_list, id_vote_type, is_unique_vote, nb_days_to_vote) VALUES (4,-1,1,1,0);
+INSERT INTO extend_rating_config (id_extender, id_mailing_list, id_vote_type, is_unique_vote, nb_days_to_vote) 
+VALUES (4,-1,1,1,0);
 
