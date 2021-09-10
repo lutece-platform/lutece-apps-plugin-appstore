50ad8977f65da09d633868e552ba345a5c98f8a5
ThomasDumont <thomas.dumont@paris.fr>
2021-09-10 11:54:10 +0200

    APPSTORE-16 : Update appstore tables creation to ensure compatibility with PostGreSQL

--- src/sql/plugins/appstore/plugin/create_db_appstore.sql
+++ src/sql/plugins/appstore/plugin/create_db_appstore.sql
@@ -5,9 +5,9 @@
 
 DROP TABLE IF EXISTS appstore_category;
 CREATE TABLE appstore_category (		
-id_category int(11) NOT NULL default '0',
+id_category int NOT NULL default '0',
 name varchar(50) NOT NULL default '',
-id_order int(11) NOT NULL default '0',
+id_order int NOT NULL default '0',
 PRIMARY KEY (id_category)
 );
 
@@ -17,12 +17,12 @@ PRIMARY KEY (id_category)
 
 DROP TABLE IF EXISTS appstore_application;
 CREATE TABLE appstore_application (		
-id_application int(11) NOT NULL default '0',
+id_application int NOT NULL default '0',
 title varchar(50) NOT NULL default '',
 description varchar(255) NOT NULL default '',
-id_category int(11) NOT NULL default '0',
-id_order int(11) NOT NULL default '0',
-id_icon int(11) NOT NULL default '0',
+id_category int NOT NULL default '0',
+id_order int NOT NULL default '0',
+id_icon int NOT NULL default '0',
 pom_url varchar(50) NOT NULL default '',
 webapp_url varchar(256) NOT NULL default '',
 sql_script_url varchar(256) NOT NULL default '',
@@ -30,8 +30,8 @@ artifact_id varchar(50) NOT NULL default '',
 presentation long varchar NULL,
 installation long varchar NULL,
 version varchar(50) NOT NULL default '',
-build_status int(11) NOT NULL default '0',
-publish_status int(11) NOT NULL default '0',
+build_status int NOT NULL default '0',
+publish_status int NOT NULL default '0',
 
 PRIMARY KEY (id_application)
 );
@@ -43,7 +43,7 @@ PRIMARY KEY (id_application)
 
 DROP TABLE IF EXISTS appstore_component;
 CREATE TABLE appstore_component (		
-id_component int(11) NOT NULL default '0',
+id_component int NOT NULL default '0',
 group_id varchar(50) NOT NULL default '',
 title varchar(50) NOT NULL default '',
 description varchar(255) NOT NULL default '',
@@ -59,8 +59,8 @@ PRIMARY KEY (id_component)
 
 DROP TABLE IF EXISTS appstore_application_component;
 CREATE TABLE appstore_application_component (		
-id_application int(11) NOT NULL default '0',
-id_component int(11) NOT NULL default '0',
+id_application int NOT NULL default '0',
+id_component int NOT NULL default '0',
 PRIMARY KEY (id_application, id_component)
 );
 
